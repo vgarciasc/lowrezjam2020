11e2b26b8790177a04578b8a25ad86eed7b9220e
vinigarcia <vingarciasc@gmail.com>
2020-08-14 19:42:37 -0300

    making portal change color when completed

--- assets/Portal.gd
+++ assets/Portal.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 export (PackedScene) var level_scene
+export (int) var level_id
 export (Direction.Dir) var player_comes_from
 
 func _ready():
-	$AnimationPlayer.play("idle")
+	if $"/root/Global".completed_levels.has(level_id):
+		$AnimationPlayer.play("idle_incomplete")
+	else:
+		$AnimationPlayer.play("idle_complete")
 
 func enter():
 	$"/root/Global".last_portal_path_entered = get_path()
